2082a2cc70bc12a8f273c0b259efd17fdf82799a
bbokser <bbokser@gmail.com>
2022-05-19 19:16:56 -0400

    Testing argparse

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
 #include"main.h"
+#include"argparse.hpp"
 
-int main() {
-    std::cout << "hello rex hopper" << std::endl; 
+int main(int argc, char *argv[]) {
+  argparse::ArgumentParser program("program_name");
 
-    return -1; 
+//   program.add_argument("square")
+//     .help("display the square of a given integer")
+//     .scan<'i', int>();
+
+//   try {
+//     program.parse_args(argc, argv);
+//   }
+//   catch (const std::runtime_error& err) {
+//     std::cerr << err.what() << std::endl;
+//     std::cerr << program;
+//     std::exit(1);
+//   }
+
+//   auto input = program.get<int>("square");
+//   std::cout << (input * input) << std::endl;
+
+  return 0;
 }
